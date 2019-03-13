eb62423854470fc62b3e3bd4938979494b398686
unknown <2575485952@qq.com>
2019-03-13 20:46:46 +0800

    水仙花

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php 
 
- echo "hello world";
+
+for($i=100;$i<1000;$i++){ 
+　　$a=intval($i/100); 
+　　$b=intval($i/10)%10; 
+　　$c=$i%10; 
+　　if(pow($a,3)+pow($b,3)+pow($c,3)==$i){ 
+　　echo $i."\t"; 
+　　} 
+　　} 
+
+
 
  ?>
